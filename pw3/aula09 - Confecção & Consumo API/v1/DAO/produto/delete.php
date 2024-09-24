8e5e4e3f70d3f865ad94078e6dfeb9e704369657
Your Name <you@example.com>
2024-09-24 11:54:43 -0300

    update: Adição da aula 9 de pw3

---
+++ pw3/aula09 - Confecção & Consumo API/v1/DAO/produto/delete.php
@@ -0,0 +1,8 @@
+<?php 
+   function deletar_usuario($conexao, $id){
+        $sql = "DELETE FROM tbl_usuarios WHERE id = $id;";
+        $res = mysqli_query($conexao, $sql) or die("Erro ao tentar deletar");
+        fecharConexao($conexao);
+        return $res;
+   };
+?>
